d76bf978667b7a1ded63a997c7fdec2f7a1442a8
enlima <everton.lima@live.com>
2023-02-04 18:44:21 -0300

    Implement ghosts' movements

--- pacman/game.py
+++ pacman/game.py
@@ -1,3 +1,5 @@
+import random
+
 from pacman.result_enum import Result
 
 
@@ -13,7 +15,7 @@ def play(game_map, key):
     if not is_there_remaining_pills(game_map):
         return Result.VICTORY
 
-    return Result.CONTINUE
+    return move_ghosts(game_map)
 
 
 def calculate_next_position(game_map, key):
@@ -65,6 +67,14 @@ def is_a_ghost(game_map, next_x_pos, next_y_pos):
     return game_map[next_x_pos][next_y_pos] == 'G'
 
 
+def is_a_pill(game_map, next_x_pos, next_y_pos):
+    return game_map[next_x_pos][next_y_pos] == 'P'
+
+
+def is_pacman(game_map, next_x_pos, next_y_pos):
+    return game_map[next_x_pos][next_y_pos] == '@'
+
+
 def is_there_remaining_pills(game_map):
     for x in range(len(game_map)):
         for y in range(len(game_map[x])):
@@ -82,3 +92,56 @@ def move_pacman(game_map, next_pacman_x, next_pacman_y):
     # moves pacman to his new position
     game_map[next_pacman_x] = game_map[next_pacman_x][0:next_pacman_y] + '@' + game_map[next_pacman_x][
                                                                                next_pacman_y + 1:]
+
+
+def move_ghosts(game_map):
+    all_ghosts = find_all_ghosts(game_map)
+
+    for ghost in all_ghosts:
+        ghost_x = ghost[0]
+        ghost_y = ghost[1]
+
+        possible_new_positions = [
+            [ghost_x, ghost_y + 1],
+            [ghost_x + 1, ghost_y],
+            [ghost_x, ghost_y - 1],
+            [ghost_x - 1, ghost_y]
+        ]
+
+        random_number = random.randint(0, 3)
+        next_ghost_x = possible_new_positions[random_number][0]
+        next_ghost_y = possible_new_positions[random_number][1]
+
+        if not is_within_borders(game_map, next_ghost_x, next_ghost_y):
+            continue
+
+        if is_a_wall(game_map, next_ghost_x, next_ghost_y):
+            continue
+
+        if is_a_ghost(game_map, next_ghost_x, next_ghost_y):
+            continue
+
+        if is_a_pill(game_map, next_ghost_x, next_ghost_y):
+            continue
+
+        if is_pacman(game_map, next_ghost_x, next_ghost_y):
+            return Result.GAME_OVER
+
+        # clears the position where ghost was
+        game_map[ghost_x] = game_map[ghost_x][0:ghost_y] + '.' + game_map[ghost_x][ghost_y + 1:]
+        # moves ghost to his new position
+        game_map[next_ghost_x] = game_map[next_ghost_x][0:next_ghost_y] + 'G' + game_map[next_ghost_x][
+                                                                                next_ghost_y + 1:]
+
+    return Result.CONTINUE
+
+
+def find_all_ghosts(game_map):
+    all_ghosts_positions = []
+
+    for x in range(len(game_map)):
+        for y in range(len(game_map[x])):
+            if game_map[x][y] == 'G':
+                all_ghosts_positions.append([x, y])
+
+    return all_ghosts_positions
